d8ccc7fae56047c6c1bd4b139eaaba564ba8e6bd
Alexander Ostrovsky <thealeos@gmail.com>
2024-10-02 22:55:28 +0400

    Add GuessTheFlag project and image assets

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexander Ostrovsky on 2/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
