753e0a954ab37c2a5e49784971ed542cab79a7f5
Alexander Ostrovsky <thealeos@gmail.com>
2024-10-05 22:01:19 +0400

    Add a project BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alexander Ostrovsky on 5/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
